1d8058976eb769ce547a56641af3cd7600fc9b2e
Eliso Saladze <87029504+wimpykido@users.noreply.github.com>
2024-08-14 00:04:33 +0400

    refactor: scanner page

--- src/pages/appreciate/scanner.tsx
+++ src/pages/appreciate/scanner.tsx
@@ -1,14 +1,13 @@
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { Scanner } from "@yudiel/react-qr-scanner";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import BackButtonAppBar from "src/components/appbar";
+import background from "src/assets/images/scanner-back.png";
 
 const ScanQrCodePage = () => {
-  const [data, setData] = useState("");
   const navigate = useNavigate();
   const handleScan = (result: string | null) => {
     if (result) {
-      setData(result);
       const url = new URL(result);
       if (
         url.hostname === "web.itu-net.com" &&
@@ -26,9 +25,89 @@ const ScanQrCodePage = () => {
 
   return (
     <Stack>
-      <Typography>Scan QR Code</Typography>
-      <Scanner onScan={(result) => handleScan(result[0].rawValue)} />
-      <Typography>{data}</Typography>
+      <BackButtonAppBar pageName="" />
+      <Stack
+        alignItems={"center"}
+        justifyContent={"center"}
+        gap={4}
+        marginX={2}
+      >
+        <Stack
+          borderRadius={4}
+          alignItems={"center"}
+          padding={1}
+          sx={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+          marginTop={10}
+        >
+          <Typography fontSize={32}>
+            Scan{" "}
+            <Typography fontSize={32} component={"span"} color={"primary"}>
+              QR Code
+            </Typography>
+          </Typography>
+          <Typography>Place QR inside the frame to scan</Typography>
+        </Stack>
+        <Box position="relative" width={300} height={300}>
+          <Scanner
+            onScan={(result) => handleScan(result[0].rawValue)}
+            components={{
+              audio: false,
+              finder: false,
+              torch: false,
+            }}
+            styles={{
+              container: {
+                width: "100%",
+                height: "100%",
+              },
+            }}
+          />
+
+          <Box position="absolute" top={0} left={0} right={0} bottom={0}>
+            <Box
+              position="absolute"
+              top={0}
+              left={0}
+              width={50}
+              height={50}
+              borderTop="4px solid #21954D"
+              borderLeft="4px solid #21954D"
+            />
+            <Box
+              position="absolute"
+              top={0}
+              right={0}
+              width={50}
+              height={50}
+              borderTop="4px solid #21954D"
+              borderRight="4px solid #21954D"
+            />
+            <Box
+              position="absolute"
+              bottom={0}
+              left={0}
+              width={50}
+              height={50}
+              borderBottom="4px solid #21954D"
+              borderLeft="4px solid #21954D"
+            />
+            <Box
+              position="absolute"
+              bottom={0}
+              right={0}
+              width={50}
+              height={50}
+              borderBottom="4px solid #21954D"
+              borderRight="4px solid #21954D"
+            />
+          </Box>
+        </Box>
+      </Stack>
     </Stack>
   );
 };
--- src/pages/postPage/index.tsx
+++ src/pages/postPage/index.tsx
@@ -1,23 +1,15 @@
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import {
-  AppBar,
-  Box,
-  IconButton,
-  Skeleton,
-  Stack,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { Box, Skeleton, Stack } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 
-import { Params, useNavigate, useParams } from "react-router-dom";
+import { Params, useParams } from "react-router-dom";
 import { getPost } from "src/api/post";
 import PostItem from "src/components/post-item";
-import notificationsIcon from "src/assets/icons/notifications.svg";
+
+import BackButtonAppBar from "src/components/appbar";
 
 const PostPage = () => {
   const { postId } = useParams<Params>();
-  const navigate = useNavigate();
+
   const post = useQuery({
     queryKey: ["postDetails", postId],
     queryFn: () => getPost(postId!),
@@ -25,27 +17,7 @@ const PostPage = () => {
 
   return (
     <Stack p={2}>
-      <AppBar>
-        <Toolbar>
-          <ArrowBackIosIcon
-            onClick={() => navigate(-1)}
-            sx={{ color: "secondary.dark" }}
-          />
-          <Box sx={{ flexGrow: 1 }}>
-            <Typography
-              fontWeight={500}
-              variant="h6"
-              component="div"
-              sx={{ textAlign: "center", color: "black", fontSize: 20 }}
-            >
-              Post
-            </Typography>
-          </Box>
-          <IconButton>
-            <img src={notificationsIcon} />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+      <BackButtonAppBar pageName="Post" />
       {post.isFetching || post.isLoading ? (
         <Box mt={2}>
           <Skeleton
