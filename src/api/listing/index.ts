3b52925e49a21e00b4e0a184d0d24fc6d72f645d
Giga Ananidze <ananidzegiga@gmail.com>
2024-08-15 16:09:38 +0400

    feat: nearby users on map page

--- src/api/listing/index.ts
+++ src/api/listing/index.ts
@@ -1,7 +1,15 @@
 import { get } from "src/lib/request/request";
-import { CurrentUser, ListingApiResponse } from "./types";
+import { CurrentUser, ListingApiResponse, LocationQueryParams } from "./types";
 
 export const getUsers = async () => get<ListingApiResponse>(`users/listing`);
 
 export const getUser = async (userId: string) =>
   get<{ status: string; user: CurrentUser }>(`users/listing/${userId}`);
+
+export const getUsersByLocation = async (params: LocationQueryParams) => {
+  const queryString = new URLSearchParams(
+    params as unknown as Record<string, string>
+  ).toString();
+
+  return get<ListingApiResponse>(`users/nearby?${queryString}`);
+};
