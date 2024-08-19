f02f9ea6c2194ef7f68965ffea9f793f14731833
Eliso Saladze <87029504+ElisoSaladze@users.noreply.github.com>
2024-08-20 03:01:36 +0400

    settings page

--- src/pages/groups/group-settings.tsx
+++ src/pages/groups/group-settings.tsx
@@ -1,27 +1,21 @@
-import { Box, Typography, List, ListItem, ListItemText, IconButton } from '@mui/material';
-import { ArrowForwardIos } from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
+import { Typography, Stack } from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+import NavigationItem from "src/components/navigation-item";
 
 const GroupSettings = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    <Box p={2}>
-      <Typography variant="h6" gutterBottom>Group Settings</Typography>
-      <List>
-        <ListItem button onClick={() => navigate('/groups/followings')}>
-          <ListItemText primary="Following" />
-          <IconButton edge="end">
-            <ArrowForwardIos />
-          </IconButton>
-        </ListItem>
-        <ListItem button onClick={() => navigate('/groups/invitations')}>
-          <ListItemText primary="Invitation" />
-          <IconButton edge="end">
-            <ArrowForwardIos />
-          </IconButton>
-        </ListItem>
-      </List>
-    </Box>
+    <Stack margin={2} alignItems={"center"}>
+      <Typography variant="h6" gutterBottom>
+        Group Settings
+      </Typography>
+      <NavigationItem onClick={() => navigate("followings")} name="Following" />
+      <NavigationItem
+        name="Invitation"
+        onClick={() => navigate("invitations")}
+      />
+    </Stack>
   );
 };
 
