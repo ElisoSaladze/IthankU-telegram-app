eb8a30920be893acd6758a875512f1f073c7cb90
Eliso <eliso.saladze5@gmail.com>
2024-08-20 16:20:01 +0400

    .

---
+++ src/pages/pending-transactions/incoming.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { getPendingTransactions } from "src/api/transaction";
+
+const Incoming = () => {
+  const { data: transactions } = useQuery({
+    queryKey: ["transactions"],
+    queryFn: () => getPendingTransactions(),
+  });
+  return <Stack></Stack>;
+};
+
+export default Incoming;
+
