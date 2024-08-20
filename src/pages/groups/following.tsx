ba19f3777eac57faeb642e6eb62b93cb442a3840
Eliso <eliso.saladze5@gmail.com>
2024-08-20 16:19:47 +0400

    refactor followings page

--- src/pages/groups/following.tsx
+++ src/pages/groups/following.tsx
@@ -1,17 +1,36 @@
-import { Typography, CircularProgress, Stack } from "@mui/material";
+import {
+  Typography,
+  CircularProgress,
+  Stack,
+  ListItemButton,
+  Avatar,
+  IconButton,
+  Box,
+} from "@mui/material";
 
-import { useQuery } from "@tanstack/react-query";
-import { userGroups } from "src/api/group";
-import GroupItem from "src/components/group-item";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+import { leaveGroup, userGroups } from "src/api/group";
+import ShadeComponent from "src/components/shade-component";
+import TagItem from "src/components/tag";
+import defaultImageUrl from "src/assets/images/itu-circle.png";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const Following = () => {
   const {
     data: groups,
     isLoading,
     isError,
+    refetch,
   } = useQuery({
     queryKey: ["groups"],
     queryFn: async () => userGroups(),
+  });
+
+  const { mutate: leave } = useMutation({
+    mutationKey: ["join-group"],
+    mutationFn: (groupId: string) => leaveGroup(groupId),
+    onSuccess: () => refetch(),
   });
 
   if (isLoading) return <CircularProgress />;
@@ -23,7 +42,59 @@ const Following = () => {
       </Typography>
 
       {groups.data.map((group) => (
-        <GroupItem key={group._id} group={group} />
+        <ListItemButton
+          sx={{
+            width: "100%",
+            borderRadius: 5,
+            padding: 1,
+            boxShadow: "0px 0px 8.2px -1px #00000026",
+          }}
+        >
+          <Stack
+            sx={{ width: "100%" }}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            direction={"row"}
+          >
+            <Stack gap={2} alignItems={"center"} direction={"row"}>
+              <Box paddingY={0.5} position={"relative"}>
+                <Avatar
+                  sx={{ width: 70, height: 70, borderRadius: 4 }}
+                  variant="rounded"
+                  src={group.groupImage || defaultImageUrl}
+                />
+                <Avatar
+                  sx={{
+                    height: 35,
+                    width: 35,
+                    bgcolor: "black",
+                    position: "absolute",
+                    bottom: -5,
+                    right: -10,
+                  }}
+                >
+                  <Typography fontSize={10} color="white">
+                    {group.membersCount}
+                  </Typography>
+                </Avatar>
+              </Box>
+              <Stack gap={0.5}>
+                <Typography fontSize={15} fontWeight={600}>
+                  {group.name}
+                </Typography>
+                <ShadeComponent color="green" name={group.shade} />
+                <Stack direction={"row"}>
+                  {group.tags?.map((tag, i) => (
+                    <TagItem key={i} tag={tag} />
+                  ))}
+                </Stack>
+              </Stack>
+            </Stack>
+            <IconButton onClick={() => leave(group._id)}>
+              <LogoutIcon fontSize="large" />
+            </IconButton>
+          </Stack>
+        </ListItemButton>
       ))}
     </Stack>
   );
