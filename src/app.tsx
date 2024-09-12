ad0a7a84683c5e675d71e482de21841d69ee6499
Makhare Makharadze <64287262+Makhare01@users.noreply.github.com>
2024-09-12 12:14:25 +0400

    fix: remove consoles and fix navigating issue (#38)

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,22 @@ import './styles/app.css';
 import { Providers } from './providers';
 import { AuthSwitch } from './app/auth-switch';
 import { Box, Typography } from '@mui/material';
+import { useNetworkStatus } from './lib/hooks';
 
 export const App = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const app = (window as any).Telegram!.WebApp;
   // Ensure the SDK script has been loaded
+
+  const { isOnline } = useNetworkStatus();
+
+  if (!isOnline) {
+    return (
+      <Box width={1} height={1} p={3}>
+        <Typography>You are offline, please check your internet connection!</Typography>
+      </Box>
+    );
+  }
 
   if (!app) {
     console.error('TelegramWebApp is not available.');
